fccebfbd8d22e89cb08817b3d594f54984e7fa01
TungNT-2810 <yugioh2810@gmail.com>
2017-04-25 12:00:33 +0700

    Update OCR

--- app/src/main/java/com/ldz/fpt/businesscardscanner/activity/FinalImageActivity.java
+++ app/src/main/java/com/ldz/fpt/businesscardscanner/activity/FinalImageActivity.java
@@ -1,17 +1,24 @@
 package com.ldz.fpt.businesscardscanner.activity;
 
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import com.googlecode.leptonica.android.Pix;
-import com.googlecode.leptonica.android.ReadFile;
-import com.googlecode.leptonica.android.Scale;
+import com.googlecode.tesseract.android.TessBaseAPI;
 import com.ldz.fpt.businesscardscanner.R;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class FinalImageActivity extends AppCompatActivity {
+    private static final String TAG = "FinalImageActivity";
     private static final int MIN_PIXEL_COUNT = 3 * 1024 * 1024;
     //view
     private ImageView imageView;
@@ -19,6 +26,10 @@ public class FinalImageActivity extends AppCompatActivity {
     public static Bitmap cropImage;
     //
     private long nativePix;
+    // OCR
+    private TessBaseAPI mTess;
+    private String datapath;
+    private String language;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +44,9 @@ public class FinalImageActivity extends AppCompatActivity {
         super.onResume();
         if (cropImage != null) {
             imageView.setImageBitmap(cropImage);
+            checkFile(new File(datapath + "tessdata/"));
+            mTess.init(datapath, language);
+            processImage();
         }
     }
 
@@ -45,6 +59,10 @@ public class FinalImageActivity extends AppCompatActivity {
     private void init() {
         //view
         imageView = (ImageView) findViewById(R.id.imv_image);
+        // OCR
+        language = "eng";
+        datapath = getFilesDir()+ "/tesseract/";
+        mTess = new TessBaseAPI();
     }
 
 //    private long getNativepix() {
@@ -60,4 +78,52 @@ public class FinalImageActivity extends AppCompatActivity {
 //        }
 //        return p.getNativePix();
 //    }
+
+    private void copyFiles() {
+        try {
+            String filepath = datapath + "/tessdata/eng.traineddata";
+            AssetManager assetManager = getAssets();
+
+            //open byte streams for reading/writing
+            InputStream instream = assetManager.open("tessdata/eng.traineddata");
+            OutputStream outstream = new FileOutputStream(filepath);
+
+            //copy the file to the location specified by filepath
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = instream.read(buffer)) != -1) {
+                outstream.write(buffer, 0, read);
+            }
+            outstream.flush();
+            outstream.close();
+            instream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void checkFile(File dir) {
+        //directory does not exist, but we can successfully create it
+        if (!dir.exists()&& dir.mkdirs()){
+            copyFiles();
+        }
+        //The directory exists, but there is no data file in it
+        if(dir.exists()) {
+            String datafilepath = datapath+ "/tessdata/eng.traineddata";
+            File datafile = new File(datafilepath);
+            if (!datafile.exists()) {
+                copyFiles();
+            }
+        }
+    }
+
+    public void processImage(){
+        String OCRresult = "";
+        mTess.setImage(cropImage);
+        OCRresult = mTess.getUTF8Text();
+        Log.d(TAG, OCRresult);
+    }
 }
